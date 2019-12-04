900b4921cc901af1c488ff591aa76db01e67e7eb
GZer <g.manafest@gmail.com>
2019-12-04 22:54:42 +1300

    removed backup

--- backup/Lvl1Reset.ps1
+++
@@ -1,4 +0,0 @@
-$origin="C:\Users\Geoffrey\Documents\GitHub\MyKolbot\Lvl1 Reset"
-$destination="C:\Users\Geoffrey\Saved Games\Diablo II"
-foreach($file in $destination){Remove-Item -Path $file -Force -Recurse}
-foreach($file in $origin){Copy-Item -Path $file -Destination $Destination -Force -Recurse}
