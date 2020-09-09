03ba76ac66323ff26090ea0a74706593a9920e45
GZer <g.manafest@gmail.com>
2020-09-09 14:15:21 +1200

    lvl1 reset

--- Lvl1Reset.ps1
+++ Lvl1Reset.ps1
@@ -1,6 +1,6 @@
-$origin="C:\Users\Geoffrey\Documents\GitHub\MyKolbot\Lvl1 Reset"
-$destination="C:\Users\Geoffrey\Saved Games\Diablo II\"
-$logs="C:\Users\Geoffrey\Documents\GitHub\MyKolbot\d2bs\kolbot\logs\*"
+$origin="C:\Users\gmcdo\Documents\MyKolbot\Lvl1Reset"
+$destination="C:\Users\gmcdo\Saved Games\Diablo II\"
+$logs="C:\Users\gmcdo\Documents\MyKolbot\d2bs\kolbot\logs\*"
 foreach($file in $destination){Remove-Item -Path $file -Force -Recurse}
 foreach($file in $logs){Remove-Item -Path $file -Force -Recurse}
 foreach($file in $origin){Copy-Item -Path $file -Destination $Destination -Force -Recurse}
